4167adaba3027743030bf2266ca820293eae6ccf
Bob Jelica <bob@jelica.se>
2011-11-27 01:41:54 +0100

    more files

--- PlexATV/Classes/PlexCompatibility.h
+++ PlexATV/Classes/PlexCompatibility.h
@@ -6,8 +6,27 @@
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
+//  BASED ON: 
+//  SMFCompatibility.h
+//  SMFramework
+//
+//  Created by Thomas Cool on 7/13/11.
+//  Copyright 2011 tomcool.org. All rights reserved.
+//
+
 #import <Foundation/Foundation.h>
 
 @interface PlexCompatibility : NSObject
-
+{
+    BOOL _usingFourPointTwo;
+    BOOL _usingFourPointThree;
+    BOOL _usingFourPointFour;
+}
++(PlexCompatibility *)compat;
+-(BOOL)usingFourPointTwo;
+-(BOOL)usingFourPointThree;
+-(BOOL)usingFourPointFour;
 @end
+
+#define PLEX_COMPAT [PlexCompatibility compat]
+
--- PlexATV/Classes/PlexCompatibility.m
+++ PlexATV/Classes/PlexCompatibility.m
@@ -6,8 +6,55 @@
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
+//  BASED ON:
+//  SMFCompatibility.m
+//  SMFramework
+//
+//  Created by Thomas Cool on 7/13/11.
+//  Copyright 2011 tomcool.org. All rights reserved.
+//
+
 #import "PlexCompatibility.h"
 
 @implementation PlexCompatibility
 
+
+SYNTHESIZE_SINGLETON_FOR_CLASS(PlexCompatibility, compat)
+-(id)init
+{
+    self=[super init];
+    if (self!=nil) {
+        Class cls = NSClassFromString(@"ATVVersionInfo");
+        
+        _usingFourPointFour=NO;
+        _usingFourPointThree=NO;
+        _usingFourPointTwo=NO;
+        if (cls != nil && [[cls currentOSVersion] isEqualToString:@"4.2"]) {
+            _usingFourPointTwo=YES;
+        }
+        if (cls != nil && [[cls currentOSVersion] isEqualToString:@"4.3"]) {
+            _usingFourPointTwo=YES;
+            _usingFourPointThree=YES;
+        }
+        if (cls != nil && [[cls currentOSVersion] isEqualToString:@"5.0"]) {
+            _usingFourPointTwo=YES;
+            _usingFourPointThree=YES;
+            _usingFourPointFour=YES;
+        }
+    }
+    return self;
+}
+-(BOOL)usingFourPointTwo
+{  
+    return _usingFourPointTwo;
+}
+-(BOOL)usingFourPointThree
+{
+    return _usingFourPointThree;
+}
+-(BOOL)usingFourPointFour
+{
+    return _usingFourPointFour;
+}
 @end
+
